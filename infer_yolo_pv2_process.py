f4aad6d6a58c9a9dc17f2d757f9bb2b38de424ac
Allan <allan.kouidri@gmail.com>
2022-10-06 14:00:28 +0200

    remove unnecessary import

--- infer_yolo_pv2_process.py
+++ infer_yolo_pv2_process.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import cv2
 import torch
 import numpy as np
 import os
