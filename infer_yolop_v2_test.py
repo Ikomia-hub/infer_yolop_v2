bac185cd331a49c635adb61b8dbc404b0399768b
Allan <allan.kouidri@gmail.com>
2022-11-21 14:06:06 +0100

    Add test file

---
+++ infer_yolop_v2_test.py
@@ -0,0 +1,15 @@
+import logging
+import cv2
+from ikomia.utils.tests import run_for_test
+
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::infer yolop v2 panoptic segmentation=====")
+    logger.info("----- Use default parameters")
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])
+    input_img_0 = t.getInput(0)
+    input_img_0.setImage(img)
+    return run_for_test(t)
